5a703647f886ddedc11ce40ff84210b48cc4355e
Renato Nascimento <renato@renatonascimento.com>
2023-11-18 10:55:15 -0300

    Initial Commit

---
+++ AleloProducts/AleloProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AleloProductsApp.swift
+//  AleloProducts
+//
+//  Created by Renato Nascimento on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AleloProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
